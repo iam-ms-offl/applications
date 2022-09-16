57b77b4924259f617998a071109162b7872e97b1
iam-ms-offl <108827660+iam-ms-offl@users.noreply.github.com>
2022-09-16 08:42:20 +0530

    Update download_YouTube_And_Play_Media_Player.py

--- download_YouTube_And_Play_Media_Player.py
+++ download_YouTube_And_Play_Media_Player.py
@@ -2,59 +2,90 @@ from pytube import YouTube
 import os
 import PySimpleGUI as sg
 import vlc
+import pytube
 
 def download_youtube_video_as_mp3():
-    yt = YouTube(str(input("Enter URL of Video: \n>> ")))
-    # yt = YouTube(https://www.youtube.com/watch?v=GXkYC8zn2Ss)
+    try:
+        yt = YouTube(str(input("Enter URL of YouTube Video link: \n>> ")))
+        # yt = YouTube(https://www.youtube.com/watch?v=GXkYC8zn2Ss)
 
-    video = yt.streams.filter(only_audio=True).first()
+        video = yt.streams.filter(only_audio=True).first()
 
-    # print("Enter destination file location")
-    # destination = str(input(">> ")) or '.'
-    # destination = "C:\Users\THIS PC\project_ET\project_ET\git_source\song\"
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    destination = os.path.join(base_dir + "/song")
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        destination = os.path.join(base_dir + "/song")
 
-    out_file = video.download(output_path=destination)
-    print(out_file)
-    print(os.path)
-    filename = out_file.split("/")
-    print(filename[-1])
+        out_file = video.download(output_path=destination)
+        filename = out_file.split("/")
 
+        print(yt.title + "has been successfully downloaded in .mp3 format")
+    except Exception as err:
+        print(err)
+        return 0
 
 
-    print(yt.title + "has been successfully downloaded in .mp3 format")
+
+
+def download_youtube_video_as_mp4():
+    try:
+        yt = YouTube(str(input("Enter URL of YouTube Video link: \n>> ")))
+        # yt = YouTube(https://www.youtube.com/watch?v=GXkYC8zn2Ss)
+
+        video = yt.streams.filter().first()
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        destination = os.path.join(base_dir + "/song")
+
+        out_file = video.download(output_path=destination)
+        filename = out_file.split("/")
+        print("Video download has been completed successfully")
+    except Exception as err:
+        print(err)
+        return 0
+
 
 def play_songs_via_mediaplayer():
-    controls = [sg.Button("Play"),sg.Button("Pause"),sg.Button("Stop")]
-    layout = [[sg.FileBrowse(key="-MP3-",enable_events=True)], controls]
-    player = None
+    try:
+        controls = [sg.Button("Play"),sg.Button("Pause"),sg.Button("Stop")]
+        layout = [[sg.FileBrowse(key="-MP3-",enable_events=True)], controls]
+        player = None
 
-    window = sg.Window("MP3 Player", layout)
+        window = sg.Window("MP3 Player", layout)
 
-    while True:
-        event, values =  window.read()
+        while True:
+            event, values =  window.read()
 
-        if event == "OK" or event == sg.WIN_CLOSED:
-            break
-        if event == "-MP3-":
-            player = vlc.MediaPlayer(values['-MP3-'])
-        if event == "Play" and player is not None:
-            player.play()
-        if event == "Pause" and player is not None:
-            player.pause()
-        if event == "Stop" and player is not None:
-            player.stop()
+            if event == "OK" or event == sg.WIN_CLOSED:
+                break
+            if event == "-MP3-":
+                player = vlc.MediaPlayer(values['-MP3-'])
+            # if event == "-MP4-":
+            #     player = vlc.MediaPlayer(values['-MP4-'])
+            if event == "Play" and player is not None:
+                player.play()
+            if event == "Pause" and player is not None:
+                player.pause()
+            if event == "Stop" and player is not None:
+                player.stop()
 
 
-    window.close()
+        window.close()
+    except Exception as err:
+        print(err)
+        return 0
+
+
 
 
 
 
 if __name__ == '__main__':
-    user_input = input("Please choose your option:  1. Download YouTube Song and Play Songs in MP3 Player, 2. Play Songs in MP3 Player. \n>> ")
+    user_input = input("Please choose your option:  1. Download YouTube Song and Play Songs in MP3 Player, 2. Download YouTube Video Song and Play. 3. Play Songs \n>> ")
     print(user_input)
     if user_input == "1":
-        download_youtube_video_as_mp3()    
-    play_songs_via_mediaplayer()
+        download_youtube_video_as_mp3()
+        play_songs_via_mediaplayer()
+    if user_input == "2":
+        download_youtube_video_as_mp4()
+        play_songs_via_mediaplayer()
+    if user_input == "3":
+        play_songs_via_mediaplayer() 
+    
